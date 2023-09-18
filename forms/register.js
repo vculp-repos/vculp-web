8a4f21478c79260a44344ab37e43903ad3e6f68d
keepitupsid <keepitup.sid@gmail.com>
2023-09-18 11:56:05 +0530

    Update register.js
    added character count js

--- forms/register.js
+++ forms/register.js
@@ -3,16 +3,16 @@ const form = document.getElementById('register-form');
 const message = document.getElementById('register-message');
 
 // Submit form using AJAX
-form.addEventListener('submit', function(event) {
+form.addEventListener('submit', function (event) {
   event.preventDefault(); // Prevent default form submission
 
   const xhr = new XMLHttpRequest();
   xhr.open('POST', form.action, true);
   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
-  xhr.onreadystatechange = function() {
+  xhr.onreadystatechange = function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
-      message.textContent = 'You details are submitted!';
+      message.textContent = 'Your details are submitted!';
       message.style.display = 'block';
       form.reset();
     } else {
@@ -23,3 +23,24 @@ form.addEventListener('submit', function(event) {
 
   xhr.send(new URLSearchParams(new FormData(form)).toString());
 });
+
+//Charcount
+{
+  var messageTextarea = document.getElementById("message");
+  var charCount = document.getElementById("char-count");
+  var maxChars = 500;
+
+  messageTextarea.addEventListener("input", function () {
+    var charLength = messageTextarea.value.length;
+
+    if (charLength > maxChars) {
+      messageTextarea.value = messageTextarea.value.substring(0, maxChars);
+      charCount.textContent = "500/500";
+      charCount.style.color = "red";
+    } else {
+      charCount.textContent = charLength + "/" + maxChars;
+      charCount.style.color = "black"; // Reset color if within the limit
+    }
+  });
+  
+};
